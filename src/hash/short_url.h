7ca7674dc24b1c29cf5ea28c8002b81d8936514a
sangwon <leeh8911@gmail.com>
2023-12-31 11:57:14 +0900

    update hash example

---
+++ src/hash/short_url.h
@@ -0,0 +1,34 @@
+﻿/*!
+ * @file short_url.h
+ * @author sangwon (leeh8911@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-12-30
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#ifndef SRC_HASH_SHORT_URL_H_
+#define SRC_HASH_SHORT_URL_H_
+
+#include <string>
+#include <unordered_map>
+
+namespace algorithm {
+class ShortUrlMap {
+ public:
+    ShortUrlMap() = default;
+    void Emplace(const std::string& short_url, const std::string& long_url) {
+        m_map.emplace(short_url, long_url);
+    }
+    std::string& operator[](const std::string& short_url) {
+        return m_map[short_url];
+    }
+
+ private:
+    std::unordered_map<std::string, std::string> m_map{};
+};
+}  // namespace algorithm
+
+#endif  // SRC_HASH_SHORT_URL_H_
---
+++ test/hash/short_url_test.cpp
@@ -0,0 +1,26 @@
+﻿/*!
+ * @file short_url_test.cpp
+ * @author sangwon (leeh8911@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-12-30
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include "hash/short_url.h"
+
+#include <gtest/gtest.h>
+
+TEST(ShortUrlTest, SampleCase) {
+    algorithm::ShortUrlMap url_map{};
+
+    url_map.Emplace("www.gg.su", "www.google.com");
+    url_map.Emplace("www.nv.su", "www.naver.com");
+    url_map.Emplace("www.kk.su", "www.kakao.com");
+
+    EXPECT_EQ(url_map["www.gg.su"], "www.google.com");
+    EXPECT_EQ(url_map["www.nv.su"], "www.naver.com");
+    EXPECT_EQ(url_map["www.kk.su"], "www.kakao.com");
+}
