b6a40230dfd3bcd95d20a6c0cb0298b189ce5bcc
sangwon lee <leeh8911@gmail.com>
2024-01-03 05:57:02 +0900

    update diviable_packing

---
+++ src/greedy/dividable_packing.h
@@ -0,0 +1,43 @@
+﻿/*!
+ * @file dividable_packing.h
+ * @author sangwon lee (leeh8911@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-01-03
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef SRC_GREEDY_DIVIDABLE_PACKING_H_
+#define SRC_GREEDY_DIVIDABLE_PACKING_H_
+
+#include <vector>
+
+namespace algorithm {
+class Object {
+ public:
+    Object(int64_t id, int64_t weight, int64_t price)
+        : m_id{id},
+          m_weight{weight},
+          m_price{price},
+          m_price_per_weight{static_cast<double>(weight) /
+                             static_cast<double>(price)} {}
+    bool operator==(const Object& other) {
+        return (m_id == other.m_id) && (m_weight == other.m_weight) &&
+               (m_price == other.m_price);
+    }
+
+ private:
+    int64_t m_id{};
+    int64_t m_weight{};
+    int64_t m_price{};
+    double m_price_per_weight{};
+};
+
+std::vector<Object> DividablePacking(std::vector<Object>& objects) {
+    return objects;
+}
+}  // namespace algorithm
+
+#endif  // SRC_GREEDY_DIVIDABLE_PACKING_H_
---
+++ test/greedy/dividable_packing_test.cpp
@@ -0,0 +1,40 @@
+﻿/*!
+ * @file dividable_packing_test.cpp
+ * @author sangwon lee (leeh8911@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-01-03
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "greedy/dividable_packing.h"
+
+#include <gtest/gtest.h>
+
+#include "util.h"
+
+TEST(DividablePackingTest, SampleCase) {
+    std::vector<int64_t> ids = {1, 2, 3, 4, 5, 6, 7};
+    std::vector<int64_t> weights = {1, 2, 5, 9, 2, 3, 4};
+    std::vector<int64_t> prices = {10, 7, 15, 10, 12, 11, 5};
+
+    std::vector<algorithm::Object> objects{};
+    objects.reserve(weights.size());
+
+    auto id_iter = ids.begin();
+    auto weight_iter = weights.begin();
+    auto price_iter = prices.begin();
+    for (; weight_iter != weights.end(); ++weight_iter, ++price_iter) {
+        objects.emplace_back(*id_iter, *weight_iter, *price_iter);
+    }
+
+    auto result = algorithm::DividablePacking(objects);
+
+    EXPECT_EQ(4, result.size());
+    EXPECT_EQ(algorithm::Object{1, 1, 10}, result[0]);
+    EXPECT_EQ(algorithm::Object{5, 2, 12}, result[1]);
+    EXPECT_EQ(algorithm::Object{6, 3, 11}, result[2]);
+    EXPECT_EQ(algorithm::Object{2, 2, 7}, result[3]);
+}
