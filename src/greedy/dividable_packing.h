29babe16504418aaecc827467fa1204fe7bbd479
sangwon lee <leeh8911@gmail.com>
2024-01-05 06:52:32 +0900

    update dividable packing

--- src/greedy/dividable_packing.h
+++ src/greedy/dividable_packing.h
@@ -12,31 +12,70 @@
 #ifndef SRC_GREEDY_DIVIDABLE_PACKING_H_
 #define SRC_GREEDY_DIVIDABLE_PACKING_H_
 
+#include <algorithm>
+#include <functional>
+#include <iostream>
 #include <vector>
 
 namespace algorithm {
 class Object {
  public:
-    Object(int64_t id, int64_t weight, int64_t price)
+    Object(int64_t id, int64_t weight, double price)
         : m_id{id},
           m_weight{weight},
           m_price{price},
-          m_price_per_weight{static_cast<double>(weight) /
-                             static_cast<double>(price)} {}
-    bool operator==(const Object& other) {
+          m_price_per_weight{static_cast<double>(weight) / price} {}
+
+    Object(const Object& other)
+        : Object(other.m_id, other.m_weight, other.m_price) {}
+
+    bool operator==(const Object& other) const {
         return (m_id == other.m_id) && (m_weight == other.m_weight) &&
                (m_price == other.m_price);
     }
 
+    bool operator<(const Object& other) const {
+        return m_price_per_weight < other.m_price_per_weight;
+    }
+
+    bool operator>(const Object& other) const {
+        return m_price_per_weight > other.m_price_per_weight;
+    }
+
+    int64_t Id() const { return m_id; }
+    int64_t Weight() const { return m_weight; }
+    double Price() const { return m_price; }
+
  private:
     int64_t m_id{};
     int64_t m_weight{};
-    int64_t m_price{};
+    double m_price{};
     double m_price_per_weight{};
 };
 
-std::vector<Object> DividablePacking(std::vector<Object>& objects) {
-    return objects;
+std::ostream& operator<<(std::ostream& os, const Object& object) {
+    os << "Object[" << object.Id() << "] {weight: " << object.Weight()
+       << ", price: " << object.Price() << "}";
+    return os;
+}
+
+std::vector<Object> DividablePacking(std::vector<Object>& objects,
+                                     int64_t capacity) {
+    std::sort(objects.begin(), objects.end(), std::less<Object>());
+
+    std::vector<Object> result{};
+
+    for (const auto& obj : objects) {
+        if (capacity > obj.Weight()) {
+            result.emplace_back(obj);
+            capacity -= obj.Weight();
+        } else {
+            result.emplace_back(obj.Id(), capacity,
+                                (obj.Price() * capacity) / obj.Weight());
+            break;
+        }
+    }
+    return result;
 }
 }  // namespace algorithm
 
--- test/greedy/dividable_packing_test.cpp
+++ test/greedy/dividable_packing_test.cpp
@@ -18,7 +18,7 @@
 TEST(DividablePackingTest, SampleCase) {
     std::vector<int64_t> ids = {1, 2, 3, 4, 5, 6, 7};
     std::vector<int64_t> weights = {1, 2, 5, 9, 2, 3, 4};
-    std::vector<int64_t> prices = {10, 7, 15, 10, 12, 11, 5};
+    std::vector<double> prices = {10., 7., 15., 10., 12., 11., 5.};
 
     std::vector<algorithm::Object> objects{};
     objects.reserve(weights.size());
@@ -26,15 +26,16 @@ TEST(DividablePackingTest, SampleCase) {
     auto id_iter = ids.begin();
     auto weight_iter = weights.begin();
     auto price_iter = prices.begin();
-    for (; weight_iter != weights.end(); ++weight_iter, ++price_iter) {
+    for (; weight_iter != weights.end();
+         ++id_iter, ++weight_iter, ++price_iter) {
         objects.emplace_back(*id_iter, *weight_iter, *price_iter);
     }
 
-    auto result = algorithm::DividablePacking(objects);
+    auto result = algorithm::DividablePacking(objects, 7);
 
     EXPECT_EQ(4, result.size());
-    EXPECT_EQ(algorithm::Object{1, 1, 10}, result[0]);
-    EXPECT_EQ(algorithm::Object{5, 2, 12}, result[1]);
-    EXPECT_EQ(algorithm::Object{6, 3, 11}, result[2]);
-    EXPECT_EQ(algorithm::Object{2, 2, 7}, result[3]);
+    EXPECT_EQ(algorithm::Object(1, 1, 10.), result[0]);
+    EXPECT_EQ(algorithm::Object(5, 2, 12.), result[1]);
+    EXPECT_EQ(algorithm::Object(6, 3, 11.), result[2]);
+    EXPECT_EQ(algorithm::Object(2, 1, 3.5), result[3]);
 }
