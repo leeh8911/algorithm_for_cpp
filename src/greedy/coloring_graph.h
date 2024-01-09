c177f2bdafc9c3a1c26463f0796f9a3d044f13a0
sangwon lee <leeh8911@gmail.com>
2024-01-10 05:54:43 +0900

    update coloring graph (failed test)

---
+++ src/greedy/coloring_graph.h
@@ -0,0 +1,37 @@
+﻿/*!
+ * @file coloring_graph.h
+ * @author sangwon lee (leeh8911@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-01-10
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef SRC_GREEDY_COLORING_GRAPH_H_
+#define SRC_GREEDY_COLORING_GRAPH_H_
+
+#include <vector>
+
+namespace algorithm {
+template <typename Color>
+class ColoringGraph {
+ public:
+    ColoringGraph() = default;
+
+    void AddEdge(int64_t from, int64_t to) {}
+
+    std::vector<Color> Nodes() const { return nodes; };
+
+ private:
+    struct Edge {
+        std::pair<int64_t, Color> from;
+        std::pair<int64_t, Color> to;
+    };
+    std::vector<Edge> edges{};
+    std::vector<Color> nodes{};
+};
+}  // namespace algorithm
+
+#endif  // SRC_GREEDY_COLORING_GRAPH_H_
---
+++ test/greedy/coloring_graph_test.cpp
@@ -0,0 +1,42 @@
+﻿/*!
+ * @file coloring_graph_test.cpp
+ * @author sangwon lee (leeh8911@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-01-10
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "greedy/coloring_graph.h"
+
+#include <gtest/gtest.h>
+
+enum class Color { Red = 1, Blue, Green, Yellow, Black, White };
+
+TEST(ColoringGraphTest, SampleCase) {
+    algorithm::ColoringGraph<Color> graph{};
+
+    graph.AddEdge(1, 2);
+    graph.AddEdge(1, 5);
+    graph.AddEdge(2, 5);
+    graph.AddEdge(2, 4);
+    graph.AddEdge(3, 4);
+    graph.AddEdge(3, 7);
+    graph.AddEdge(4, 5);
+    graph.AddEdge(4, 6);
+    graph.AddEdge(4, 8);
+    graph.AddEdge(5, 8);
+    graph.AddEdge(6, 8);
+
+    auto nodes = graph.Nodes();
+    EXPECT_EQ(Color::Red, nodes[0]);
+    EXPECT_EQ(Color::Blue, nodes[1]);
+    EXPECT_EQ(Color::Red, nodes[2]);
+    EXPECT_EQ(Color::Green, nodes[3]);
+    EXPECT_EQ(Color::Yellow, nodes[4]);
+    EXPECT_EQ(Color::Red, nodes[5]);
+    EXPECT_EQ(Color::Blue, nodes[6]);
+    EXPECT_EQ(Color::Blue, nodes[7]);
+}
